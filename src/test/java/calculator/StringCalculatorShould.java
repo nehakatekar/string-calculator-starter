14a586e5e94ac836ef7b2f3209467d177d082b4d
nehakatekar <nehakatekar230@gmail.com>
2021-10-17 19:13:18 +0530

    Test cases passed

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,16 +5,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
+	
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+	}
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, StringCalculator.add(""));
+	}
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, StringCalculator.add("1"));
+	}
+	
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, StringCalculator.add("1,2"));
+	}
+
+	@Test
+	public void testThreeNumbers() {
+		assertEquals(6, StringCalculator.add("1,2,3"));
+	}
+	
+	@Test
+	public void testNewLine() {
+		assertEquals(6, StringCalculator.add("1\n2,3"));
+	}
 }
